a6f373e9cca814b3f59f14f69a2f5d6e9d5d8f71
AmitDeka <amitdek49@gmail.com>
2024-02-29 20:46:48 +0530

    Fix: All

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,13 +7,14 @@ getUserData = async (username) => {
   // console.log(userData);
 
   // console.log(response);
-  // if (response.status === 404) {
   if (!response.ok) {
     userDetailsCard = `
-    <div class="card_user_notfound">
-      <img src="./assets/image/not_found.svg" alt="Not found" />
-      <h1>User details not found</h1>
-      <p>Please check the username and try again.</p>
+    <div class="card_user">
+      <div class="card_user_notfound">
+        <img src="./assets/image/not_found.svg" alt="Not found" />
+        <h1>User details not found</h1>
+        <p>Please check the username and try again.</p>
+      </div>
     </div>
     `;
   } else {
@@ -41,64 +42,66 @@ getUserData = async (username) => {
       : "<span>Not Available</span>";
 
     userDetailsCard = `
-    <div class="card_user_image">
-        <img
-            src="${userData.avatar_url}"
-            alt="User Image" />
-    </div>
-    <div class="card_user_info">
-        <div class="card_user_info_header">
-            <div>
-                <h1 class="card_user_info_header_name">
-                    ${bioName}
-                </h1>
-                <a
-                  class="card_user_info_header_username"
-                  href="${userData.html_url}"
-                  >@${userData.login}</a
-                >
-            </div>
-            <div>
-              <p class="card_user_info_header_date">
-                Joined <span>${formattedDate}</span>
-              </p>
-            </div>
-        </div>
-        <div class="card_user_info_bio">
-          <p>${bioText}</p>
-        </div>
-        <div class="card_user_info_repo">
-            <div>
-                <h2>Repos</h2>
-                <p>${userData.public_repos}</p>
-            </div>
-            <div>
-                <h2>Followers</h2>
-                <p>${userData.followers}</p>
-            </div>
-            <div>
-                <h2>Followings</h2>
-                <p>${userData.following}</p>
-            </div>
-            </div>
-        <div class="card_user_info_details">
-            <div>
-                <i class="ri-map-pin-fill"></i>
-                <p>${bioAddr}</p>
-            </div>
-            <div>
-                <i class="ri-twitter-x-fill"></i>
-                <p>${bioTwitter}</p>
-            </div>
-            <div>
-                <i class="ri-links-fill"></i>
-                <p>${bioBlogLink}</p>
-            </div>
-            <div>
-                <i class="ri-building-fill"></i>
-                <p>${bioCompany}</p>
-            </div>
-        </div>
+    <div class="card_user">
+      <div class="card_user_image">
+          <img
+              src="${userData.avatar_url}"
+              alt="User Image" />
+      </div>
+      <div class="card_user_info">
+          <div class="card_user_info_header">
+              <div>
+                  <h1 class="card_user_info_header_name">
+                      ${bioName}
+                  </h1>
+                  <a
+                    class="card_user_info_header_username"
+                    href="${userData.html_url}"
+                    >@${userData.login}</a
+                  >
+              </div>
+              <div>
+                <p class="card_user_info_header_date">
+                  Joined <span>${formattedDate}</span>
+                </p>
+              </div>
+          </div>
+          <div class="card_user_info_bio">
+            <p>${bioText}</p>
+          </div>
+          <div class="card_user_info_repo">
+              <div>
+                  <h2>Repos</h2>
+                  <p>${userData.public_repos}</p>
+              </div>
+              <div>
+                  <h2>Followers</h2>
+                  <p>${userData.followers}</p>
+              </div>
+              <div>
+                  <h2>Followings</h2>
+                  <p>${userData.following}</p>
+              </div>
+              </div>
+          <div class="card_user_info_details">
+              <div>
+                  <i class="ri-map-pin-fill"></i>
+                  <p>${bioAddr}</p>
+              </div>
+              <div>
+                  <i class="ri-twitter-x-fill"></i>
+                  <p>${bioTwitter}</p>
+              </div>
+              <div>
+                  <i class="ri-links-fill"></i>
+                  <p>${bioBlogLink}</p>
+              </div>
+              <div>
+                  <i class="ri-building-fill"></i>
+                  <p>${bioCompany}</p>
+              </div>
+          </div>
+      </div>
     </div>
   `;
   }
